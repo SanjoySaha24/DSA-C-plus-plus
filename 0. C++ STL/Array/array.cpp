3f605c1d950045ee6f89560926920cdcc68727a0
Sanjoy Saha <sanjoysaha0524@gmail.com>
2022-02-02 00:23:26 +0530

    STL array

--- 0. C++ STL/Array/array.cpp
+++ 0. C++ STL/Array/array.cpp
@@ -3,12 +3,16 @@
 
 using namespace std;
 int main(){
+    
+   // int basic[3] = {1,2,3}; // taking input usually
 
-    array<int,4> a = {1,2,3,4};
+    array<int,4> a = {1,2,3,4}; // stl creating array
+
     int size = a.size();
+    cout<<"The size is - "<<size<<endl; // returns size of array
 
     for(int i=0; i<size; i++){
-        cout<<a[i]<<endl;
+        cout<<a[i]<<endl; // returns output of input array
     }
 
     cout<<"Element at 2nd index - "<<a.at(2)<<endl;
@@ -18,5 +22,9 @@ int main(){
     cout<<"First Element - "<<a.front()<<endl;
 
     cout<<"Last element - "<<a.back()<<endl;
+
+    
+
+
        
 }
---
+++ 0. C++ STL/Array/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+cout<<"Element at 2nd index - "<<a.at(2)<<endl;
+
+    cout<<"Empty or not - "<<a.empty()<<endl;
+
+    cout<<"First Element - "<<a.front()<<endl;
+
+    cout<<"Last element - "<<a.back()<<endl;
