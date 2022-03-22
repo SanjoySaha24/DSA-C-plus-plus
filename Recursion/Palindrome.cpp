3cd1dcf04948705b6b1ccf487b80d852cf7abca4
Sanjoy Saha <76898740+SanjoySaha24@users.noreply.github.com>
2022-03-22 08:28:35 +0530

    rec

--- Recursion/Palindrome.cpp
+++ Recursion/Palindrome.cpp
@@ -1,16 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool palindrome(string str, int start, int end){
-    if(start >= end)
+bool palindrome(string str, int start, int end)
+{
+    if (start >= end)
         return true;
 
-        return((str[start] == str[end]) &&
-        palindrome(str, start+1, end-1)); 
-}
-int main(){
-    string s = "bob";
-    printf("%s", palindrome(s,0,s.length() - 1) ? "true" : "false");
+    if (str[start] != str[end])
+        return false;
 
-    return 0;
+    return palindrome(str, start + 1, end - 1);
 }
+int main()
+{
+    string str = "bob";
+    int len = str.length();
+    if (palindrome(str, 0, len - 1))
+    {
+        cout << "Palindrome";
+    }
+    else
+        cout << "Not Palindrome";
+        }
--- Recursion/sum.cpp
+++ Recursion/sum.cpp
@@ -1,3 +1,5 @@
+// Non-recursive
+
 #include<bits/stdc++.h>
 using namespace std;
 
