9aa876102c43b13237dfa7e485f72bf8e75c8180
Sanjoy Saha <76898740+SanjoySaha24@users.noreply.github.com>
2022-06-07 10:12:18 +0530

    .

---
+++ BASICS/functions.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printHello(){
+    cout<<"hello World";
+}
+
+int sum(int a, int b){
+    int result = a+b;
+    return result;
+}
+
+int main(){
+    cout<<"The sum is : "<<sum(2,3);
+}
