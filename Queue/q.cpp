1b38abb0766fd38fefcd0269d05fb573acafd546
Sanjoy Saha <sanjoysaha0524@gmail.com>
2023-08-09 16:45:19 +0530

    file updated

---
+++ Queue/q.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    queue<int> q;
+
+    q.push(11);
+    q.push(15);
+    q.push(13);
+    cout<<"size of queue is : "<<q.size()<<endl;
+
+    q.pop();
+            cout<<"size of queue is : "<<q.size()<<endl;
+
+}
